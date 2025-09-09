3a2d6dc3c135eac3bb9d004133848ca1142a79bc
Ukhanyo1 <khanyowest01@gmail.com>
2025-09-09 13:58:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -114,6 +114,22 @@ document.getElementById("contactForm").addEventListener("submit", function(e) {
   this.reset();
 });
 
+// Scroll to Top Button
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+window.onscroll = function() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    scrollTopBtn.style.display = "block";
+  } else {
+    scrollTopBtn.style.display = "none";
+  }
+};
+
+scrollTopBtn.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
+
 
 
 
